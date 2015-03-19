0cb025e28bad83001b903ce6379b2149587bb55b
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-18 22:21:22 -0700

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Vineet Tiwari on 2015-03-18.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Vineet Tiwari on 2015-03-18.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
