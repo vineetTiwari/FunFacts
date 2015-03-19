1d7052024a1fae89ef8589a62bd441e74a13eb6f
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-19 01:42:44 -0700

    added the facts property

--- FunFacts/ViewController.h
+++ FunFacts/ViewController.h
@@ -10,6 +10,9 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *funFactLabel;
+
+@property (strong, nonatomic) NSArray *facts;
 
 @end
 
--- FunFacts/ViewController.m
+++ FunFacts/ViewController.m
@@ -17,11 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+	
+	self.facts = [[NSArray alloc] initWithObjects:@"Ants stretch when they wake up", @"Ostraches can run faster than horses", nil];
+	
+	self.funFactLabel.text = [self.facts objectAtIndex:0];
+	
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)showFunFact {
+	
+	self.funFactLabel.text = [self.facts objectAtIndex:1];
+	
+}
+
 
 @end
